ed2bc308e9702805f0efb5fe71be8f6d6c7f8ae8
Juanita Smith <juanita_smith@goodyear.com>
2023-03-20 15:56:20 +0000

    exploration improvements

--- src/utils.py
+++ src/utils.py
@@ -57,6 +57,17 @@ def convert_time(col):
 
     # convert column to datetime
 #     col = pd.to_datetime(col , format='%H%M', errors='raise')   
-    col = pd.to_datetime(col, format='%H%M') - pd.to_datetime(col, format='%H%M').normalize()    
+#     col = pd.to_datetime(col, format='%H%M') - pd.to_datetime(col, format='%H%M').normalize()    
     
-    return col        
+    return col   
+
+def change_width(ax, new_value) :
+    for patch in ax.patches :
+        current_width = patch.get_width()
+        diff = current_width - new_value
+
+        # we change the bar width
+        patch.set_width(new_value)
+
+        # we recenter the bar
+        patch.set_x(patch.get_x() + diff * .5)
